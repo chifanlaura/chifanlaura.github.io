0c459def5d6489ad2a2409c7c950a7073d569ba4
chifanlaura <lauralaury24@gmail.com>
2021-04-22 04:42:38 +0200

    istorie

--- scripts/index.js
+++ scripts/index.js
@@ -55,3 +55,19 @@ function toggleStreetView() {
     panorama.setVisible(false);
   }
 }
+
+function showMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("moreBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
